dacb9763f2e8a2b963d15308661caa29e4bc1858
Jesper <jespert90@gmail.com>
2023-01-26 23:47:57 +0100

    working on room to room travel

--- src/Game.java
+++ src/Game.java
@@ -1,11 +1,17 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Game {
     public static void game() {
+// Controls
         Scanner input = new Scanner(System.in);
         String command = input.next();
+
+        if (Objects.equals(command, "north")){
+
+        }
 
 // Rooms Archive - To be moved somewhere
         Room room2=new Room (2,"Second Room","This is the starter description for the second room","A1");
@@ -13,6 +19,11 @@ public class Game {
         List<Room> rooms = new ArrayList<>();
         rooms.add(room1);
         rooms.add(room2);
+
+// Current Room
+       Room Current = room1;
+
+
 
     }
 
