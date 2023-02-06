f75edb44d064aec849f38f77ef21f256598fd483
Jesper <jespert90@gmail.com>
2023-02-07 00:42:40 +0100

    working on parser

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,30 @@
 package src;
 
-public class Main {  // Main is sort of a playground right now
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
     public static void main(String[] args){
-        Game.game();
+
+    Scanner scanner = new Scanner(System.in);
+
+    String input = scanner.next();
+
+        System.out.println(stringDivider(input));
+
+      }
+      static String stringDivider(String input){
+        //List<String> resultList = List.of(input.trim().split(" ")); Thought maybe ArrayList was the problem
+          String[] resultList = input.split("\\s+");
+
+
+        for (String s : resultList){
+            System.out.println(s);
+        }
+
+        return "done";
 
       }
 }
