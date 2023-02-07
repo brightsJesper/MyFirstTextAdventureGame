87e5c1d093eddc704107cbd5b0cde950dd5fa076
Jesper <jespert90@gmail.com>
2023-02-07 23:09:27 +0100

    working on parser some more

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,19 @@
 package src;
 
-import java.util.List;
-import java.util.Scanner;
-
 public class Main {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        Game.game();
+    }
+/*
         Scanner scanner = new Scanner(System.in);
 
         String input = scanner.nextLine();
 
         System.out.println(Parser.strDivider((input)));
 
+    */
+
 
 
         }
-    }
--- src/Parser.java
+++ src/Parser.java
@@ -4,20 +4,28 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Parser {
-    static List<String> strDivider(String input) {
-        input = input.toLowerCase();
-        return List.of(input.split(" "));
+    static List<String> strDivider(List<String> list) {
+        return strCounter(list);
     }
+
+    //method figuring how many arguments that were used and redirects the string accordingly
     static String strCounter(List<String> list) {
-        if (list.size()==2){
-            strVerbAdjective(list);
-        }
-        return "strCounter done";
-    }
-    static void strVerbAdjective(List<String> list){
-        switch (list.get(0)){
-            case "go":
+        if (list.size() == 2) {
 
         }
+        return strVerbAdjective(list);
     }
+
+    // switch method dealing with simple verb/adjective commands.
+    static void strVerbAdjective(List<String> list) {
+        switch (list.get(0)) {
+            case "go":
+                switch (list.get(1)) {
+                    case "north":
+                        System.out.println("You walk north");
+
+
+                }
+        }
     }
+}
