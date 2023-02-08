5e64bbd127689012e997d788140ca676a6dcdc19
Jesper <jesper.thuresson@brightstraining.com>
2023-02-08 11:25:37 +0100

    Storing rooms in Vault class to fetch and set them in Game

--- src/Parser.java
+++ src/Parser.java
@@ -1,31 +1,22 @@
 package src;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
 public class Parser {
-    static List<String> strDivider(List<String> list) {
-        return strCounter(list);
-    }
-
-    //method figuring how many arguments that were used and redirects the string accordingly
-    static String strCounter(List<String> list) {
+    static void in(String input) {
+        List<String> list = List.of(input.split(" "));
         if (list.size() == 2) {
+            switch (list.get(0)) {
+                case "go":
+                    switch (list.get(1)) {
+                        case "north":
+                            System.out.println("You walk north");
+                    }
+            }
 
-        }
-        return strVerbAdjective(list);
-    }
-
-    // switch method dealing with simple verb/adjective commands.
-    static void strVerbAdjective(List<String> list) {
-        switch (list.get(0)) {
-            case "go":
-                switch (list.get(1)) {
-                    case "north":
-                        System.out.println("You walk north");
-
-
-                }
         }
     }
 }
