8324174ca07e513c722fd506808bf2ab0aa1ce5a
Jesper <jesper.thuresson@brightstraining.com>
2023-02-08 17:32:16 +0100

    Storing rooms in Vault class to fetch and set them in Game

--- src/Game.java
+++ src/Game.java
@@ -6,23 +6,9 @@ import java.util.Objects;
 import java.util.Scanner;
 
 public class Game {
-
-    public static Room here;
-    public static List<Room> rooms = new ArrayList<>();
-
-
     public static void game() {
-
-// Rooms Archive - To be moved somewhere
-        Room room2=new Room ("Second Room","This is the starter description for the second room");
-        Room room1=new Room ("Starting Room","This is the starter description for the first room");
-        rooms.add(room1);
-        rooms.add(room2);
-// Current Room
- here = room1;
-
+        Vault.init();
 // Testing Stuff
-
 
     }
 }
--- src/Vault.java
+++ src/Vault.java
@@ -1,5 +1,31 @@
 package src;
 
-public class Vault {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Vault {
+    public static Room here;
+    public static List<Room> rooms = new ArrayList<>();
+    public static void roomArchive(){
+        Room room2=new Room ("Second Room","This is the starter description for the second room");
+        Room room1=new Room ("Starting Room","This is the starter description for the first room");
+        rooms.add(room2);
+        rooms.add(room1);
+    }
+
+    public static Room getHere() {
+        return here;
+    }
+
+    public static void setHere(Room here) {
+        Vault.here = here;
+    }
+
+    public static List<Room> getRooms() {
+        return rooms;
+    }
+
+    public static void setRooms(List<Room> rooms) {
+        Vault.rooms = rooms;
+    }
 }
