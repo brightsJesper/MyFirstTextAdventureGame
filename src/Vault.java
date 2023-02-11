af0615c414ddb432863acd8c3f1b23782053a481
Jesper <jesper.thuresson@brightstraining.com>
2023-02-11 13:56:14 +0100

    Working on roomArchive logic

--- src/Vault.java
+++ src/Vault.java
@@ -5,12 +5,15 @@ import java.util.List;
 
 public class Vault {
     private static Room here;
-    private static List<Room> rooms = new ArrayList<>();
-    public static void roomArchive(){
+    public static ArrayList<Room> roomArchive(){
         Room room2=new Room ("Second Room","This is the starter description for the second room");
         Room room1=new Room ("Starting Room","This is the starter description for the first room");
+
+        List<Room> rooms = new ArrayList<>();
+
         rooms.add(room2);
         rooms.add(room1);
+        return rooms
     }
 
     public static Room getHere() {
